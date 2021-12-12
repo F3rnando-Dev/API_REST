14eb1cff60b345671b5a2e3cd15295ca6a7b862f
Fernando Santana <fernando_92-@hotmail.com>
2021-12-12 15:13:54 -0300

    CRUD ok

---
+++ src/main/java/crudlab/apirestlab/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package crudlab.apirestlab.repository;
+
+import crudlab.apirestlab.model.UsuarioModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<UsuarioModel, Integer>{
+}
